89475ea69d73b9286f5c2472694bce641e83d106
SorrowAkoji <sorrow.akoji@gmail.com>
2026-06-15 10:46:39 +0700

    link fix

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -12,7 +12,7 @@ async function getScheduleEvents(): Promise<{
 }> {
   try {
     console.log(process.env.NEXT_PUBLIC_BASE_URL);
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/schedule`, {
+    const res = await fetch(`https://sorrowakoji.vercel.app/api/schedule`, {
       next: { revalidate: 300 },
     })
 
