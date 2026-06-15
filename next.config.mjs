8e61749bdd39140c350ffccbb9f884cce5718d2a
SorrowAkoji <sorrow.akoji@gmail.com>
2026-06-15 13:08:02 +0700

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,13 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+        pathname: '/**',
+      },
+    ],
   },
   allowedDevOrigins: ['192.168.137.1'],
 }
