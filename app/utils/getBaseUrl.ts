c201a5fd8f13c70277f1e75b6b2a453c1de597f5
SorrowAkoji <sorrow.akoji@gmail.com>
2026-06-27 08:17:42 +0700

    baseurl

--- app/utils/getBaseUrl.ts
+++ app/utils/getBaseUrl.ts
@@ -1,6 +1,6 @@
 export function getBaseUrl() {
   if (process.env.NODE_ENV === "development") {
-    return "http://localhost:3001";
+    return "http://localhost:3000";
   }
 
   return process.env.API_URL!;
